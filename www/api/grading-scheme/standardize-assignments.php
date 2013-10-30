d6d070164f2ecd08091e271a57bf990147db5e9a
Seth Battis <SethBattis@stmarksschool.org>
2013-10-30 13:44:51 -0400

    Nested Pagination
    Making use of the nested pagination. Untested.
    
    Signed-off-by: Seth Battis <SethBattis@stmarksschool.org>

--- www/api/grading-scheme/standardize-assignments.php
+++ www/api/grading-scheme/standardize-assignments.php
@@ -8,35 +8,34 @@ require_once('../page-generator.inc.php');
 
 debugFlag('START');
 
+
+$courseApi = new CanvasApiProcess(CANVAS_API_URL, CANVAS_API_TOKEN);
+$assignmentApi = new CanvasApiProcess(CANVAS_API_URL, CANVAS_API_TOKEN);
+
 /* get all courses in our Academics sub-account */
-$courses = callCanvasApiPaginated(
-	CANVAS_API_GET,
-	'/accounts/' . AFFECTED_SUBACCOUNT_ID . '/courses' // TODO make this a GET parameter?
-);
+$courses = $courseApi->get('/accounts/' . AFFECTED_SUBACCOUNT_ID . '/courses'); // TODO make this a GET parameter?
 
 do {
 
 	foreach($courses as $course) {
-		// FIXME: need to paginate assignments
-		$assignments = callCanvasApi(
-			CANVAS_API_GET,
-			"/courses/{$course['id']}/assignments"
-		);
+		$assignments = $assignmentApi->get("/courses/{$course['id']}/assignments");
 		
-		foreach ($assignments as $assignment) {
-			if ($assignment['grading_type'] == 'letter_grade' && !isset($assignment['grading_standard_id'])) {
-				$assignment = callCanvasApi(
-					CANVAS_API_PUT,
-					"/courses/{$course['id']}/assignments/{$assignment['id']}",
-					array(
-						'assignment[grading_standard_id]' => STANDARD_GRADING_SCHEME_ID // the preferred standard grading scheme
-					)
-				);
+		do {
+			foreach ($assignments as $assignment) {
+				if ($assignment['grading_type'] == 'letter_grade' && !isset($assignment['grading_standard_id'])) {
+					$assignment = callCanvasApi(
+						CANVAS_API_PUT,
+						"/courses/{$course['id']}/assignments/{$assignment['id']}",
+						array(
+							'assignment[grading_standard_id]' => STANDARD_GRADING_SCHEME_ID // the preferred standard grading scheme
+						)
+					);
+				}
 			}
-		}
+		} while ($assignments = $assignmentApi->nextPage());
 	}
 	
-} while ($courses = callCanvasApiNextPage());
+} while ($courses = $courseApi->nextPage());
 
 debugFlag('FINISH');
 
